169058fa2fd365424fca32da0b24206c3e4d6d8f
kai <137367860+Itzsoham@users.noreply.github.com>
2025-02-01 23:43:18 +0530

    project 1 setup

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Root() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Link href="/project1">Project 1</Link>
+    </div>
+  );
+}
